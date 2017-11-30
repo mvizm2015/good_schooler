b081f21e3dac96b21cadc9c483b07155891c90ad
mvizm2015 <mariaviz2015@gmail.com>
2017-11-30 16:40:41 -0500

    model user unique email

--- db/migrate/20171130163930_change_email_not_null_in_user.rb
+++ db/migrate/20171130163930_change_email_not_null_in_user.rb
@@ -1,5 +1,16 @@
 class ChangeEmailNotNullInUser < ActiveRecord::Migration[5.1]
-  def change
+  def up
+    # set a default email for all users without
+
+    User.where(email: nil).each do |user|
+      user.email = "#{user.name}@wyncode.co"
+      user.save
+    end
+
     change_column :users, :email, :string, null: false
+  end
+
+  def down
+    change_column :users, :email, :string, null: true
   end
 end
